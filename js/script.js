e8577d74bd246958ce33961553b7358d8ba1164a
Lawrence Holmes <holmes.lr@gmail.com>
2014-05-19 19:56:29 +0100

    added code which sizes the video header to the size of window minus the header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-	
+
+	var view = {
+		h: $(window).height(),
+	},
+	titleHeight = $('#title').outerHeight(),
+	videoHeight = view.h-titleHeight;
+
+	function position() {
+		$('#videohead').css('height', videoHeight);
+	}
+
+	function init() {
+		position();
+	}
+
+	init();
+
 })
